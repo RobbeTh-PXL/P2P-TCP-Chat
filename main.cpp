ed621445757b60df55baf307cdd01cedcf64a3bf
RobbeTh-PXL <12102842@student.pxl.be>
2024-02-21 19:13:07 +0100

    Added Program flow

--- main.cpp
+++ main.cpp
@@ -1,22 +1,47 @@
 #include "mainwindow.h"
 
+#include <QDebug>
 #include <QApplication>
+#include <QCommandLineParser>
 
-//TCP
+#include "cli_parser.h"
 #include "p2p_network.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-	//TCP
+	// CLI Parser
+	cli_parser parser;
+
+	// Process args
+	if (!parser.parse(a.arguments()))
+	{
+		return -1;
+	}
+
+	ParsedArguments parsedArgs = parser.getParsed_arguments();
+
+	// Network
 	p2p_network network;
 
-	network.connection_listener();
+	// Set vars
+	network.setServer_ip(parsedArgs.server_ip);
+	network.setServer_port(parsedArgs.server_port);
 
-	//network.join_network();
+	if (parsedArgs.initial_connection)
+	{
+		network.join_network();
+		network.connection_listener();
+	}
+
+	else
+	{
+		network.connection_listener();
+	}
 
     MainWindow w;
     w.show();
+
     return a.exec();
 }
